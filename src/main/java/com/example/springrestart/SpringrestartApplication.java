493f370366d13dbc0a6287f6a172e01b2b22d118
lillebitteditte <ditt2047@stud.kea.dk>
2023-09-04 10:51:57 +0200

    Initial commit

---
+++ src/main/java/com/example/springrestart/SpringrestartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrestart/SpringrestartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
